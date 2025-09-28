e8a55d75f0f6663f45a8efd6fe8d6e1615184098
Pandya Jeet Akshaykumar <jeetpandya2006@gmail.com>
2025-09-28 12:31:36 +0000

    CMake Data

---
+++ build/CMakeFiles/scrooge_coin.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/scrooge_coin.dir/src/Transaction.cpp.o"
+  "CMakeFiles/scrooge_coin.dir/src/Transaction.cpp.o.d"
+  "CMakeFiles/scrooge_coin.dir/src/TxHandler.cpp.o"
+  "CMakeFiles/scrooge_coin.dir/src/TxHandler.cpp.o.d"
+  "CMakeFiles/scrooge_coin.dir/src/UTXOPool.cpp.o"
+  "CMakeFiles/scrooge_coin.dir/src/UTXOPool.cpp.o.d"
+  "CMakeFiles/scrooge_coin.dir/src/main.cpp.o"
+  "CMakeFiles/scrooge_coin.dir/src/main.cpp.o.d"
+  "scrooge_coin"
+  "scrooge_coin.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/scrooge_coin.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/scrooge_coin.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for scrooge_coin.
+# This may be replaced when dependencies are built.
